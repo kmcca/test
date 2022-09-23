0f788968be3277505179ad1faf5e2d135628b6ff
Kevin McCarthy <kmccarthy@veracode.com>
2022-09-23 17:33:20 -0400

    broken link update

---
+++ my-website/.docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require('C:\\users\\kmccarthy\\repos\\km-docusaurus-test\\my-website\\node_modules\\infima\\dist\\css\\default\\default.css'),
+  require('C:\\users\\kmccarthy\\repos\\km-docusaurus-test\\my-website\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages'),
+  require('C:\\users\\kmccarthy\\repos\\km-docusaurus-test\\my-website\\node_modules\\@docusaurus\\theme-classic\\lib\\nprogress'),
+  require('C:\\users\\kmccarthy\\repos\\km-docusaurus-test\\my-website\\src\\css\\custom.css'),
+];
